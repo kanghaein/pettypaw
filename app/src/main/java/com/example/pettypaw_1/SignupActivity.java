2f27801521a044053a46e738d556056c89560efd
kminzy <55342113+kminzy@users.noreply.github.com>
2021-03-28 20:10:45 +0900

    #2021.03.28.20:09 #수정

--- app/src/main/java/com/example/pettypaw_1/SignupActivity.java
+++ app/src/main/java/com/example/pettypaw_1/SignupActivity.java
@@ -2,7 +2,10 @@ package com.example.pettypaw_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button button3 = findViewById(R.id.button3); //로그인
+
+        button3.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),enrollment.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/pettypaw_1/enrollment1.java
+++ app/src/main/java/com/example/pettypaw_1/enrollment1.java
@@ -1,4 +1,14 @@
 package com.example.pettypaw_1;
 
-public class enrollment1 {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class enrollment1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enrollment1);
+    }
 }
