e7316f1e22e065774ad6b706867072938a89c606
hikan <hikang1998@naver.com>
2021-03-28 17:29:09 +0900

    #2021.03.28  17:28

--- app/src/main/java/com/example/pettypaw_1/MainActivity.java
+++ app/src/main/java/com/example/pettypaw_1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.pettypaw_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1); //로그인
+        Button button2 = findViewById(R.id.button2); //회원가입
+
+        button1.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),enrollment.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
