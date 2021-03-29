a98aaa45b044689691015090d343a9d6e6929044
kminzy <55342113+kminzy@users.noreply.github.com>
2021-03-29 23:59:31 +0900

    #2021.03.29 23:58 #Layout들 추가

---
+++ app/src/main/java/com/example/pettypaw_1/CurrentCalendar.java
@@ -0,0 +1,14 @@
+package com.example.pettypaw_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrentCalendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_calendar);
+    }
+}
