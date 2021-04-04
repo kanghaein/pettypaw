ef3f9cc61e9cc35d0d27e059c0162fcc8f493a44
hyemin cho <wandukong0908@naver.com>
2021-04-04 21:50:47 +0900

    알람

--- app/src/main/java/com/example/pettypaw_1/MainActivity.java
+++ app/src/main/java/com/example/pettypaw_1/MainActivity.java
@@ -8,6 +8,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+//알람//
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.os.Build;
+import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+//
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -24,8 +37,19 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //알람
+
+    private TimePicker timePicker;
+    private AlarmManager alarmManager;
+    private int hour, minute;
+    CheckBox cbSun, cbMon, cbTue, cbWed, cbThu, cbFri, cbSat;
+
+    static String TAG="MainActivity";
+
     // 화면 간 전환을 위한 REQUEST_CODE_SIGN 설정
     public static final int REQUEST_CODE_SIGN = 101;
+
 
     // 레이아웃의 id 값들 선언
     EditText lg_ID, lg_PW;
@@ -36,6 +60,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //알람
+        timePicker=findViewById(R.id.tp_timepicker);
+        alarmManager= (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+
+        cbSun=findViewById(R.id.cb_sun);
+        cbMon=findViewById(R.id.cb_mon);
+        cbTue=findViewById(R.id.cb_thu);
+        cbWed=findViewById(R.id.cb_wed);
+        cbThu=findViewById(R.id.cb_thu);
+        cbFri=findViewById(R.id.cb_fri);
+        cbSat=findViewById(R.id.cb_sat);
 
 
         // 레이아웃 id 값들을 불러와 변수에 저장
@@ -107,6 +143,60 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void regist(View view) {
+
+        boolean[] week = { false, cbSun.isChecked(), cbMon.isChecked(), cbTue.isChecked(), cbWed.isChecked(),
+                cbThu.isChecked(), cbFri.isChecked(), cbSat.isChecked() }; // cbSun을 1번부터 사용하기 위해 배열 0번은 false로 고정
+
+        if(!cbSun.isChecked() &&  !cbMon.isChecked() &&  !cbTue.isChecked() && !cbWed.isChecked() &&  !cbThu.isChecked() && !cbFri.isChecked() && !cbSat.isChecked()){
+            Toast.makeText(this, "요일을 선택해 주세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            hour=timePicker.getHour();
+            minute=timePicker.getMinute();
+        }else{
+            Toast.makeText(this, "버전을 확인해 주세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent intent = new Intent(this, AlarmSetting.class);
+        intent.putExtra("weekday", week);
+        PendingIntent pIntent = PendingIntent.getBroadcast(this, 0,intent, 0); //PendingIntent.FLAG_UPDATE_CURRENT
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        Date today = new Date();
+        long intervalDay = 24 * 60 * 60 * 1000;// 24시간
+
+        long selectTime=calendar.getTimeInMillis();
+        long currenTime=System.currentTimeMillis();
+
+        //설정한 시간<현재 시간 알람이 다음날 울리게 설정
+        if(currenTime>selectTime){
+            selectTime += intervalDay;
+        }
+
+        Log.e(TAG,"등록 버튼을 누른 시간 : "+today+"  설정한 시간 : "+calendar.getTime());
+
+        Log.d(TAG,"calendar.getTimeInMillis()  : "+calendar.getTimeInMillis());
+
+        // 지정한 시간에 매일 알림
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, selectTime,  intervalDay, pIntent);
+
+    }// regist()
+
+    public void unregist(View view) {
+        Intent intent = new Intent(this, AlarmSetting.class);
+        PendingIntent pIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+        alarmManager.cancel(pIntent);
+    }// unregist()
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
